03cbba9a3121ecf0e7cbd8835f0047bdb1acee82
Fatih Ay <fay@k1m32s02.42istanbul.com.tr>
2022-10-14 16:49:58 +0300

    s

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,86 +6,37 @@
 /*   By: fay <marvin@42.fr>                         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/07 14:35:22 by fay               #+#    #+#             */
-/*   Updated: 2022/10/14 12:41:27 by fay              ###   ########.fr       */
+/*   Updated: 2022/10/14 16:43:01 by fay              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <stdio.h>
 #include <stdlib.h>
 #include "libft.h"
-/*
-int	ft_size(char const *s1, char const *set)
-{
-	int	x;
-	int	y;
-	int	a;
 
-	a = 0;
-	y = 0;
-	x = 0;
-	while (s1[x] != '\0')
-	{
-		while (set[y] != '\0')
-		{
-			if (s1[x] == set[y])
-			{
-				x++;
-				y = 0;
-			}
-			y++;
-		}
-		y = 0;
-		x++;
-		a++;
-	}
-	return (a);
+static char    *ft_printer(char const *s, unsigned int start, size_t len)
+{
+    char *s1;
+    int x;
+    size_t t;
+
+    t = ft_strlen(s);
+    x = 0;
+    if (start <  t)
+    {
+      s1 =(char *)malloc((len) * sizeof(char) + 1);
+        while (s[start] != '\0' && len > 0)
+        {
+            s1[x] = s[start];
+            start++;
+            len--;
+            x++;
+        }
+    }
+    else
+      s1 = malloc(1);
+    s1[x] = '\0';
+    return (s1);
 }
-
-int	ft_bsort(char const *s1, char const *set, int x)
-{
-	int	y;
-
-	y = 0;
-	while (s1[x] != '\0')
-	{
-		while (set[y] != '\0')
-		{
-			if (s1[x] == set[y])
-			{
-				x++;
-				y = 0;
-				return (0);
-			}
-			y++;
-		}
-		return (1);
-	}
-	return (0);
-}
-
-char	*ft_strtrim(char const *s1, char const *set)
-{
-	int		size;
-	char	*s2;
-	int		x;
-	int		a;
-
-	x = 0;
-	a = 0;
-	size = ft_size(s1, set);
-	s2 = malloc(size * sizeof(char) + 1);
-	while (s1[x] != '\0')
-	{
-		if (ft_bsort(s1, set, x) == 1)
-		{
-			s2[a] = s1[x];
-			a++;
-		}
-		x++;
-	}
-	s2[a] = '\0';
-	return (s2);
-}*/
-
 char    *ft_strtrim(char const *s1, char const *set)
 {
     char *a;
@@ -95,6 +46,6 @@ char    *ft_strtrim(char const *s1, char const *set)
     len = ft_strlen(s1);
     while (ft_strchr(set, s1[len]) && len)
         len--;
-    a = ft_substr((char *)s1, 0, len + 1);
+    a = ft_printer((char *)s1, 0, len + 1);
     return (a);
 }
--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: fay <fay@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/07 12:22:32 by fay               #+#    #+#             */
-/*   Updated: 2022/10/14 14:01:31 by fay              ###   ########.fr       */
+/*   Updated: 2022/10/14 16:26:53 by fay              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,17 +20,19 @@ char    *ft_substr(char const *s, unsigned int start, size_t len)
 
     t = ft_strlen(s);
     x = 0;
-    s1 = malloc(len * sizeof(char) + 1);
-	if (start <  t)
+    if (start <  t)
     {
-    	while (s[start] != '\0' && len != 0)
-    	{
-        	s1[x] = s[start];
-        	start++;
-        	len--;
-        	x++;
-    	}
-	}
-	s1[x] = '\0';
+      s1 =(char *)malloc((t-start) * sizeof(char) + 1);
+        while (s[start] != '\0' && len > 0)
+        {
+            s1[x] = s[start];
+            start++;
+            len--;
+            x++;
+        }
+    }
+    else
+      s1 = malloc(1);
+    s1[x] = '\0';
     return (s1);
 }
