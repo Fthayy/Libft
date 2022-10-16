02310ab8641cb0c87691b9f9bc58aea7e683c595
Fatih Ay <fay@k1m33s02.42istanbul.com.tr>
2022-10-16 18:47:14 +0300

    bb

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,24 +6,25 @@
 /*   By: fay <marvin@42.fr>                         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/11 11:55:03 by fay               #+#    #+#             */
-/*   Updated: 2022/10/12 16:31:30 by fay              ###   ########.fr       */
+/*   Updated: 2022/10/16 18:43:20 by fay              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_strncmp(char *s1, char *s2, unsigned int n)
+int	ft_strncmp(const char *s1, const char *s2, unsigned int n)
 {
 	unsigned int	i;
-
+	
 	i = 0;
 	if (n == 0)
 		return (0);
 	while (i < n)
 	{
-		if (s1[i] - s2[i] > 0)
-			return (1);
-		else if (s1[i] - s2[i] < 0)
-			return (-1);
-		i++;
-	}
+		if (s1[i] && s1[i] == s2[i])
+		{
+			while (s1[i] && s1[i] == s2[i] && i < n)
+				i++;
+		}
+		else
+			return ((unsigned char)s1[i] - (unsigned char)s2[i]);	}
 	return (0);
 }
