9769108192d9d1d4be8b0a8152656c95a09ca91e
Fatih Ay <fay@k1m30s06.42istanbul.com.tr>
2022-10-18 15:38:02 +0300

    lst

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: fay <marvin@42.fr>                         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/04 18:18:03 by fay               #+#    #+#             */
-/*   Updated: 2022/10/18 15:29:28 by fay              ###   ########.fr       */
+/*   Updated: 2022/10/18 15:37:40 by fay              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
@@ -19,7 +19,7 @@ void	*ft_memchr(const void *s, int c, size_t n)
 	c = (unsigned char)c;
 	s1 = (unsigned char *)s;
 	b = 0;
-	while (x < n)
+	while (b < n)
 	{
 		if (s1[b] == c)
 			return ((void *)s1 + b);
